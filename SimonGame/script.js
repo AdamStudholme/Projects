ddbe3e9db995f34463a6c0ee69f597a2ecb3e564
Adam Studholme <adam.studholme@version1.com>
2023-11-24 17:03:35 +0000

    Update: play and style. Added message: win/lose

--- SimonGame/script.js
+++ SimonGame/script.js
@@ -10,6 +10,7 @@ const btnGenerator = () => {
   return Math.trunc(Math.random() * 4);
 };
 const winningScore = 20;
+const gameEndEl = document.querySelector('.game-end');
 
 const init = () => {
   guesses = [];
@@ -17,51 +18,17 @@ const init = () => {
   score = 0;
   time = 1000;
   document.getElementById('score').textContent = 0;
-  document.getElementById('high-score').textContent = 0;
   gameStarted = false;
 };
 init();
 
-// Add event listeners to buttons
-for (let i = 0; i < btnEls.length; i++) {
-  btnEls[i].setAttribute('style', `background-color: rgb(${colors[i]});`);
-  btnEls[i].addEventListener('click', () => {
-    flash(i);
-    guesses = guesses.concat(Number(btnEls[i].id.substring(5)));
-    // Check if last guess matches the sequence
-    if (guesses[guesses.length - 1] !== sequence[guesses.length - 1]) {
-      init();
-      return console.log(`you loose`);
-    }
-    //If its the end of the sequence add another and play new sequence
-    if (guesses.length === sequence.length) {
-      time = time * 0.9;
-      if (score === winningScore) {
-        init();
-        console.log('You win!');
-      }
-      increaseSequence();
-      playSequence();
-
-      // Update score accordingly
-      score = document.getElementById('score').textContent = guesses.length;
-      console.log(score);
-
-      if (score > highscore) {
-        highscore = score;
-        document.getElementById('high-score').textContent = highscore;
-      }
-      //Reset guess array
-      guesses = [];
-    }
-  });
-}
+document.getElementById('high-score').textContent = 0;
 
 // Make button i flash
 const flash = i => {
   let selectedBtn = document.getElementById(`btn--${[i]}`);
   //btnEls[i].classList.add('clicked');
-  console.log(selectedBtn);
+  //console.log(selectedBtn);
   selectedBtn.style.boxShadow = `inset rgba(255,255,255, 0.5) 0px 0px 50px 30px`; //`0px 0px 80px 50px rgba(${colors[i]}, 0.5)`;
   selectedBtn.style.opacity = '150%';
   setTimeout(() => {
@@ -71,7 +38,50 @@ const flash = i => {
   }, time);
 };
 
-//generate sequence and play
+// Function for game sequence
+const startGame = function (i) {
+  flash(i);
+  guesses = guesses.concat(Number(btnEls[i].id.substring(5)));
+  // Check if last guess matches the sequence
+  if (guesses[guesses.length - 1] !== sequence[guesses.length - 1]) {
+    init();
+    gameEndEl.textContent = 'You Loose';
+    gameEndEl.classList.remove('hidden');
+    return;
+  }
+
+  // Update score accordingly
+  score = document.getElementById('score').textContent = guesses.length;
+  //console.log(score);
+
+  if (score > highscore) {
+    highscore = score;
+    document.getElementById('high-score').textContent = highscore;
+  }
+  //If its the end of the sequence add another light and play new sequence
+  if (guesses.length === sequence.length) {
+    time = time * 0.9;
+    if (score === winningScore) {
+      init();
+      gameEndEl.textContent = 'You Win!';
+      gameEndEl.classList.remove('hidden');
+      return;
+    }
+    increaseSequence();
+    playSequence();
+
+    //Reset guess array
+    guesses = [];
+  }
+};
+
+// Add event listeners to buttons
+for (let i = 0; i < btnEls.length; i++) {
+  btnEls[i].setAttribute('style', `background-color: rgb(${colors[i]});`);
+  btnEls[i].addEventListener('click', () => startGame(i));
+}
+
+//increase game sequence
 const increaseSequence = () => {
   sequence = sequence.concat(btnGenerator());
 };
@@ -83,7 +93,7 @@ const playSequence = () => {
     flash(sequence[squenceEl]);
     squenceEl++;
     if (squenceEl < sequence.length) {
-      console.log(sequence);
+      //console.log(sequence);
       playSequence(); // call function again with new value of sequenceEl until all el are called
     } else {
       squenceEl = 0;
@@ -94,6 +104,8 @@ const playSequence = () => {
 startEl.addEventListener('click', () => {
   if (!gameStarted) {
     gameStarted = true;
+    console.log(gameEndEl);
+    gameEndEl.classList.add('hidden');
     increaseSequence();
     playSequence();
   }
